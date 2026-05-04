836167bad292e479f54669e1278cff74239f4575
milionascimento-maker <milio.nascimento@gmail.com>
2026-05-04 10:19:35 -0300

    Refactor bot initialization and message responses
    Updated bot configuration and message handling logic.

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const bot = new Client({
     authStrategy: new LocalAuth({
         dataPath: './.wwebjs_auth'
     }),
-    puppeteer: {
+   puppeteer: {
         headless: true,
         executablePath: './chrome-linux/chrome',
         args: [
@@ -24,7 +24,7 @@ const bot = new Client({
             '--no-zygote',
             '--single-process',
             '--disable-gpu',
-            '--disable-extensions' // Deixa mais leve
+            '--user-data-dir=/tmp/puppeteer_user_data' // Força uma sessão limpa
         ]
     }
 });
